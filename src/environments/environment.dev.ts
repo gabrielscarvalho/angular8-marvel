3520dd198e2fad910309855318e84df4df3f1822
Gabriel Santos Carvalho <gabrielsc@ciandt.com>
2021-01-15 18:39:37 -0300

    Structuring - Add marvel keys

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,9 +1,9 @@
 export const environment = {
-  production: true,
+  production: false,
   api: {
     marvel: {
       url: 'https://gateway.marvel.com:443',
       key: '8623fc37ce54b25f8302bf4ea3f4c7af'
     }
   }
-};
+}
