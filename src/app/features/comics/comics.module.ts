bb9f97089a7366a9bda8923a63eb4bef3eeb6e90
Gabriel Santos Carvalho <gabrielsc@ciandt.com>
2021-01-17 11:56:57 -0300

    Creating list generic component - CSS

--- src/app/features/comics/comics.module.ts
+++ src/app/features/comics/comics.module.ts
@@ -2,18 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ComicsRoutingModule } from './comics-routing.module';
-import { ListComponent } from './components/list/list.component';
 import { ListPageComponent } from './page/list-page/list-page.component';
 import { MarvelModule } from '@app/shared/marvel/marvel.module';
 import { ComicService } from './services/comic.service';
+import { ListModule } from '@app/shared/components/list/list.module';
 
 
 @NgModule({
-  declarations: [ListComponent, ListPageComponent],
+  declarations: [ListPageComponent],
   imports: [
     CommonModule,
     ComicsRoutingModule,
-    MarvelModule
+    MarvelModule,
+    ListModule
   ],
   providers: [ ComicService ]
 })
