fa699c8ebf0d340f7391fb212b1a90210fa9cb03
Gabriel Santos Carvalho <gabrielsc@ciandt.com>
2021-01-17 16:00:47 -0300

    added filter - wip

--- src/app/shared/components/list/list.module.ts
+++ src/app/shared/components/list/list.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list.component';
 import { ListCardComponent } from './card/list.card.component';
+import { ListFilterComponent } from './filter/list.filter.component';
 
 
 
 @NgModule({
-  declarations: [ListComponent, ListCardComponent],
+  declarations: [ListComponent, ListCardComponent, ListFilterComponent],
   imports: [
     CommonModule
   ],
-  exports: [ListComponent, ListCardComponent]
+  exports: [ListComponent, ListCardComponent, ListFilterComponent]
 })
 export class ListModule { }
